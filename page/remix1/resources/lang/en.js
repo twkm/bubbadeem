3f5de89d709acef4f236946958c112271ebf58eb
Troy Meier <tm@twkm.ca>
2025-12-30 16:25:43 -0700

    Add new remix

---
+++ page/remix1/resources/lang/en.js
@@ -0,0 +1,13 @@
+var language = {
+    'start' : 'START',
+    'paused' : 'PAUSED',
+    'game' : 'MONSTAGAME!',
+    'score' : 'Score',
+    'score_per_second': 'SPS',
+    'highest_score_per_second': 'Highest SPS',
+    'resume' : 'Resume',
+    'restart' : 'New Game',
+    'quit' : 'Quit',
+    'made_with' : 'Made with ',
+    'by' : 'by'
+}
